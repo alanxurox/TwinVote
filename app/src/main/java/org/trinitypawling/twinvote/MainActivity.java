1db32d78ca5471cdce7cae0cf4ae56e6340dc285
alanxurox <alanxu2013@gmail.com>
2019-10-22 10:50:48 -0400

    Added Firebase Support: test using real-time database console.

--- app/src/main/java/org/trinitypawling/twinvote/MainActivity.java
+++ app/src/main/java/org/trinitypawling/twinvote/MainActivity.java
@@ -1,14 +1,45 @@
 package org.trinitypawling.twinvote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mChildRef = mRootRef.child("bestTwin");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.bestTwin);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mChildRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String bestTwin = dataSnapshot.getValue(String.class);
+                mTextView.setText(bestTwin);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
